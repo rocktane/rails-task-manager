63ce27a3881801b5d2af5a52a292abb4f2bf4ca3
Yohan Gouiran <y.gouiran@gmail.com>
2023-11-28 15:17:15 +0100

    done with refactoring and bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -18,19 +18,15 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
 
     redirect_to task_path(@task)
   end
 
-  def delete
-    @task = Task.find(params[:id])
+  def destroy
     @task.destroy
 
     redirect_to tasks_path
@@ -40,5 +36,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,19 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "tasks", to: 'tasks#index'
+  # get "tasks", to: 'tasks#index'
 
-  get "tasks/new", to: 'tasks#new', as:'new'
-  post "tasks", to: 'tasks#create'
+  # get "tasks/new", to: 'tasks#new', as:'new'
+  # post "tasks", to: 'tasks#create'
 
-  get "tasks/:id", to: 'tasks#show', as:'task'
+  # get "tasks/:id", to: 'tasks#show', as:'task'
 
-  get "tasks/:id/edit", to: 'tasks#edit', as:'edit'
-  patch "tasks/:id", to: 'tasks#update'
+  # get "tasks/:id/edit", to: 'tasks#edit', as:'edit'
+  # patch "tasks/:id", to: 'tasks#update'
 
-  delete "tasks/:id", to: 'tasks#delete', as:'delete'
+  # delete "tasks/:id", to: 'tasks#delete', as:'delete'
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :tasks
 end
